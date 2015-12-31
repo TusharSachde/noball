1ec5060a7c8dfb1afa22245a7b832f60a70f253d
Harsh Thakkar <harshthakkar46@gmail.com>
2015-12-31 18:48:51 +0530

    changes

--- js/controllers.js
+++ js/controllers.js
@@ -183,6 +183,19 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
       isFirstOpen: true,
       isFirstDisabled: false
     };
+
+    $scope.testimonial = [
+      {
+        img: "img/manan.png",
+        name: "manan vora",
+        descp: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+      },
+      {
+        img: "img/manan.png",
+        name: "Viraj",
+        descp: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+      }
+    ];
   })
   .controller('TermsConditionsCtrl', function($scope, TemplateService, NavigationService, $timeout) {
     //Used to name the .html file
