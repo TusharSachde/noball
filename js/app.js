4ef6a37da56c4928ffef425fdd6a2eb3aea5d759
sohan1 <shn619@gmail.com>
2016-01-07 11:04:05 +0530

    chnages

--- js/app.js
+++ js/app.js
@@ -6,143 +6,148 @@ var firstapp = angular.module('firstapp', [
   'navigationservice'
 ]);
 
-firstapp.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
 
-    // for http request with session
-    $httpProvider.defaults.withCredentials = true;
+  // for http request with session
+  $httpProvider.defaults.withCredentials = true;
 
-    $stateProvider
+  $stateProvider
 
-        .state('home', {
-        url: "/home",
-        templateUrl: "views/template.html",
-        controller: 'HomeCtrl'
+    .state('home', {
+    url: "/home",
+    templateUrl: "views/template.html",
+    controller: 'HomeCtrl'
+  })
+
+  .state('categories', {
+      url: "/categories",
+      templateUrl: "views/template.html",
+      controller: 'CategoriesCtrl'
+    })
+    .state('custom', {
+      url: "/custom",
+      templateUrl: "views/template.html",
+      controller: 'CustomCtrl'
     })
 
-    .state('categories', {
-            url: "/categories",
-            templateUrl: "views/template.html",
-            controller: 'CategoriesCtrl'
-        })
-        .state('custom', {
-            url: "/custom",
-            templateUrl: "views/template.html",
-            controller: 'CustomCtrl'
-        })
+  .state('categories-inside', {
+    url: "/categories/sweatshirts",
+    templateUrl: "views/template.html",
+    controller: 'CategoriesInsideCtrl'
+  })
 
-    .state('categories-inside', {
-        url: "/categories/sweatshirts",
-        templateUrl: "views/template.html",
-        controller: 'CategoriesInsideCtrl'
+  .state('privacypolicy', {
+      url: "/privacypolicy",
+      templateUrl: "views/template.html",
+      controller: 'PrivacyPolicyCtrl'
+    })
+    .state('review', {
+      url: "/review",
+      templateUrl: "views/template.html",
+      controller: 'ReviewCtrl'
+    })
+    .state('ready', {
+      url: "/ready",
+      templateUrl: "views/template.html",
+      controller: 'ReadyCtrl'
     })
 
-    .state('privacypolicy', {
-            url: "/privacypolicy",
-            templateUrl: "views/template.html",
-            controller: 'PrivacyPolicyCtrl'
-        })
-        .state('review', {
-            url: "/review",
-            templateUrl: "views/template.html",
-            controller: 'ReviewCtrl'
-        })
-        .state('ready', {
-            url: "/ready",
-            templateUrl: "views/template.html",
-            controller: 'ReadyCtrl'
-        })
-
-    .state('cart', {
-            url: "/cart",
-            templateUrl: "views/template.html",
-            controller: 'CartCtrl'
-        })
-        .state('profile', {
-            url: "/profile",
-            templateUrl: "views/template.html",
-            controller: 'ProfileCtrl'
-        })
-        .state('product-detail', {
-            url: "/categories/sweatshirts/product-detail",
-            templateUrl: "views/template.html",
-            controller: 'Product-DetailCtrl'
-        })
-        .state('savedesign', {
-            url: "/save-design",
-            templateUrl: "views/template.html",
-            controller: 'SaveDesignCtrl'
-        })
-        .state('termsconditions', {
-            url: "/terms-conditions",
-            templateUrl: "views/template.html",
-            controller: 'TermsConditionsCtrl'
-        })
-        .state('about', {
-            url: "/about",
-            templateUrl: "views/template.html",
-            controller: 'AboutCtrl'
-        })
-        .state('contact', {
-            url: "/contact",
-            templateUrl: "views/template.html",
-            controller: 'ContactCtrl'
-        })
-        .state('wishlist', {
-            url: "/wishlist",
-            templateUrl: "views/template.html",
-            controller: 'WishlistCtrl'
-        })
-        .state('order', {
-            url: "/order",
-            templateUrl: "views/template.html",
-            controller: 'OrderCtrl'
-        })
-
-    .state('checkout', {
-        url: "/checkout",
-        templateUrl: "views/template.html",
-        controller: 'CheckoutCtrl'
+  .state('cart', {
+      url: "/cart",
+      templateUrl: "views/template.html",
+      controller: 'CartCtrl'
+    })
+    .state('profile', {
+      url: "/profile",
+      templateUrl: "views/template.html",
+      controller: 'ProfileCtrl'
+    })
+    .state('product-detail', {
+      url: "/categories/sweatshirts/product-detail",
+      templateUrl: "views/template.html",
+      controller: 'Product-DetailCtrl'
+    })
+    .state('savedesign', {
+      url: "/save-design",
+      templateUrl: "views/template.html",
+      controller: 'SaveDesignCtrl'
+    })
+    .state('termsconditions', {
+      url: "/terms-conditions",
+      templateUrl: "views/template.html",
+      controller: 'TermsConditionsCtrl'
+    })
+    .state('about', {
+      url: "/about",
+      templateUrl: "views/template.html",
+      controller: 'AboutCtrl'
+    })
+    .state('contact', {
+      url: "/contact",
+      templateUrl: "views/template.html",
+      controller: 'ContactCtrl'
+    })
+    .state('wishlist', {
+      url: "/wishlist",
+      templateUrl: "views/template.html",
+      controller: 'WishlistCtrl'
+    })
+    .state('order', {
+      url: "/order",
+      templateUrl: "views/template.html",
+      controller: 'OrderCtrl'
     })
 
-    .state('mywishlist', {
-        url: "/mywishlist",
-        templateUrl: "views/template.html",
-        controller: 'MyWishListCtrl'
+  .state('checkout', {
+    url: "/checkout",
+    templateUrl: "views/template.html",
+    controller: 'CheckoutCtrl'
+  })
+
+  .state('mywishlist', {
+      url: "/mywishlist",
+      templateUrl: "views/template.html",
+      controller: 'MyWishListCtrl'
+    })
+    .state('thank', {
+      url: "/thank",
+      templateUrl: "views/template.html",
+      controller: 'ThankCtrl'
     })
 
-    $urlRouterProvider.otherwise("/home");
+  $urlRouterProvider.otherwise("/home");
 
 });
 
 
-firstapp.filter('serverimage', function () {
-	return function (input) {
-		if (input) {
-			return  "img/" + input;
-			// return  "http://wohlig.co.in/newfynx/uploads/" + input;
-		} else {
-			return "img/logo.png";
-		}
-	};
+firstapp.filter('serverimage', function() {
+  return function(input) {
+    if (input) {
+      return "img/" + input;
+      // return  "http://wohlig.co.in/newfynx/uploads/" + input;
+    } else {
+      return "img/logo.png";
+    }
+  };
 });
 
 
-firstapp.directive('img', function ($compile, $parse) {
-    return {
-        restrict: 'E',
-        replace: false,
-        link: function ($scope, element, attrs) {
-            var $element = $(element);
-            if (!attrs.noloading) {
-                $element.after("<img src='img/loading.gif' class='loading' />");
-                var $loading = $element.next(".loading");
-                $element.load(function () {
-                    $loading.remove();
-                    $(this).addClass("doneLoading");
-                });
-            } else {
-                $($element).addClass("doneLoading");
-            }
-        }
-    };
+firstapp.directive('img', function($compile, $parse) {
+  return {
+    restrict: 'E',
+    replace: false,
+    link: function($scope, element, attrs) {
+      var $element = $(element);
+      if (!attrs.noloading) {
+        $element.after("<img src='img/loading.gif' class='loading' />");
+        var $loading = $element.next(".loading");
+        $element.load(function() {
+          $loading.remove();
+          $(this).addClass("doneLoading");
+        });
+      } else {
+        $($element).addClass("doneLoading");
+      }
+    }
+  };
 });
