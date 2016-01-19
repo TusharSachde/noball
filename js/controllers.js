75071154104760727bfb1a11bc7f9ed98e301bd0
Rohan Gada <gadarohan17@gmail.com>
2016-01-19 15:59:52 +0530

    changes

--- js/controllers.js
+++ js/controllers.js
@@ -132,6 +132,7 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
             msg: 'Removed successfully'
           });
           $scope.getCart();
+          myfunction();
         }
       })
     };
