f2d15cbe0890bb0139504e1c320e1fa5970f6ffb
Harsh Thakkar <harshthakkar46@gmail.com>
2017-01-14 11:27:47 +0530

    ball changes

--- js/controllers.js
+++ js/controllers.js
@@ -883,6 +883,80 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
         $scope.navigation = NavigationService.getnav();
         var check = 1;
 
+        // slider
+        $scope.rslider = {
+            min: 10,
+            max: 100
+        };
+
+        $scope.ball = {};
+        $scope.ballText = {
+            top: {
+                text: 'a',
+                size: 20,
+                spacing: 1
+            },
+            middle: {
+                text: 'b',
+                size: 20,
+                spacing: 1
+            },
+            bottom: {
+                text: 'c',
+                size: 20,
+                spacing: 1
+            }
+        };
+
+        $scope.ballQuantity = 1;
+
+        $scope.changeText = function(variable) {
+            if (variable === 'top') {
+                $scope.texta = 'active';
+                $scope.textb = '';
+                $scope.textc = '';
+                $scope.textmodel = $scope.ballText.top.text;
+                $scope.sizemodel = $scope.ballText.top.size;
+                $scope.spacingmodel = $scope.ballText.top.spacing;
+            } else if (variable === 'middle') {
+                $scope.texta = '';
+                $scope.textb = 'active';
+                $scope.textc = '';
+                $scope.textmodel = $scope.ballText.middle.text;
+                $scope.sizemodel = $scope.ballText.middle.size;
+                $scope.spacingmodel = $scope.ballText.middle.spacing;
+            } else if (variable === 'bottom') {
+                $scope.texta = '';
+                $scope.textb = '';
+                $scope.textc = 'active';
+                $scope.textmodel = $scope.ballText.bottom.text;
+                $scope.sizemodel = $scope.ballText.bottom.size;
+                $scope.spacingmodel = $scope.ballText.bottom.spacing;
+            }
+        }
+
+        $scope.changeText('top');
+
+        $scope.changeSize = function(key) {
+            $scope.ballText.top.border = "1px solid #ccc";
+        };
+        $scope.resetSize = function(key) {
+            $scope.ballText.top.border = "none";
+            $scope.$apply();
+        };
+
+        $scope.switchFrontBack = function(front) {
+            $scope.ball.front = front;
+            $scope.ball.back = !front;
+            if (front) {
+                $scope.ball.cloth = 'img/ball/front.png';
+            } else {
+                $scope.ball.cloth = 'img/ball/back.png';
+            }
+        }
+
+        $scope.switchFrontBack(true);
+
         $scope.UploadTeamLogo = function() {
             check = 2;
             $uibModal.open({
@@ -959,209 +1033,6 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
         $scope.teamloging = function() {
             $scope.tab = "teamlogo";
         }
-        $scope.$on('$viewContentLoaded', function(event) {
-            $timeout(function() {
-                var scene, camera, renderer, width, height, controls, light, loader, texture, geometry, material, mesh;
-                var three = document.getElementsByClassName("threed-ball");
-                init();
-                animate();
-
-                function init() {
-                    scene = new THREE.Scene();
-                    width = 400;
-                    height = 400;
-
-                    renderer = new THREE.WebGLRenderer({
-                        antialias: true,
-                        alpha: true
-                    });
-                    renderer.setSize(width, height);
-                    document.getElementById("threed-ball").appendChild(renderer.domElement);
-                    renderer.setClearColor(0xFFFFFF, 1);
-
-                    camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
-                    camera.position.set(0, 0, 4.5);
-                    scene.add(camera);
-
-                    controls = new THREE.OrbitControls(camera, renderer.domElement);
-                    controls.enableZoom = false;
-
-                    var amblight = new THREE.AmbientLight(0xFFFFFF);
-                    scene.add(amblight);
-
-                    var dirlight = new THREE.DirectionalLight(0xFFFFFF, 0.5);
-                    dirlight.castShadow = true;
-
-                    var spotlight = new THREE.SpotLight(0xFFFFFF);
-                    spotlight.position.set(1000, 1000, 1000);
-                    spotlight.castShadow = true;
-                    camera.add(spotlight);
-
-                    geometry = new THREE.SphereGeometry(1, 50, 50);
-                    texture = new THREE.TextureLoader();
-                    texture.load(
-                        'img/textures/ball_texture.jpg',
-                        function(texture) {
-                            material = new THREE.MeshPhongMaterial({
-                                map: texture
-                            });
-                            var sphere = new THREE.Mesh(geometry, material);
-                            sphere.castShadow = true;
-                            sphere.rotation.x = 1;
-                            sphere.rotation.y = -5.5;
-                            sphere.rotation.z = -1;
-                            scene.add(sphere);
-                        },
-                        function(xhr) {
-                            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-                        },
-                        function(xhr) {
-                            console.log('An error happened');
-                        }
-                    );
-                }
-
-                var canvas = document.createElement("canvas");
-                canvas.width = 1000;
-                canvas.height = 667;
-                var c = canvas.getContext("2d");
-
-                function readURL(input) {
-                    if (input.files && input.files[0]) {
-                        var reader = new FileReader();
-                        reader.onload = function(e) {
-                            $('#ball_logo').attr('src', e.target.result);
-                            src = $('#ball_logo').attr('src');
-                            var image = new Image();
-                            image.src = src;
-                            var selectImg = '';
-                            var canvas = document.createElement("canvas");
-                            var ctx = canvas.getContext("2d");
-                            var canvasx = document.createElement("canvas");
-                            var ctxx = canvasx.getContext("2d");
-                            var originalPixels, currentPixels = null;
-                            var color, fullimg = '';
-                            canvas.width = canvasx.width = 1000;
-                            canvas.height = canvasx.height = 667;
-
-                            function HexToRGB(Hex) {
-                                var Long = parseInt(Hex.replace(/^#/, ""), 16);
-                                return {
-                                    R: (Long >>> 16) & 0xff,
-                                    G: (Long >>> 8) & 0xff,
-                                    B: Long & 0xff
-                                };
-                            }
-
-                            function fillColor(path) {
-                                color = path;
-                                if (!originalPixels) return;
-                                var newColor = HexToRGB(color);
-                                for (var I = 0, L = originalPixels.data.length; I < L; I += 4) {
-                                    if (currentPixels.data[I + 3] > 0) {
-                                        currentPixels.data[I] = newColor.R;
-                                        currentPixels.data[I + 1] = newColor.G;
-                                        currentPixels.data[I + 2] = newColor.B;
-                                    }
-                                }
-
-                                var cann = document.createElement("canvas");
-                                cann.width = selectImg.width;
-                                cann.height = selectImg.height;
-                                var ctc = cann.getContext("2d");
-                                ctc.putImageData(currentPixels, 0, 0);
-                                var newImm = new Image();
-                                newImm.src = cann.toDataURL("image/png");
-                                var imageSize = 250;
-                                var newImmWidth = newImm.width;
-                                var newImmHeight = newImm.height;
-                                var newImmWidthQu = newImmWidth / imageSize;
-                                var newImmHeightQu = newImmHeight / imageSize;
-                                var newImmWidthDp = 300 * newImmWidthQu;
-                                var newImmHeightDp = 300 * newImmHeightQu;
-                                var exWidth = (imageSize - newImmWidth) / 2;
-                                var exHeight = (imageSize - newImmHeight) / 2;
-
-                                if (newImmWidth == imageSize && newImmHeight == imageSize) {
-                                    ctx.drawImage(newImm, 0, 0, 300, 300, 10, 120, imageSize, imageSize);
-                                } else if (newImmWidth < imageSize && newImmHeight == imageSize) {
-                                    ctx.drawImage(newImm, 0, 0, 300, 300, (10 + exWidth), 120, imageSize, imageSize);
-                                } else if (newImmWidth == imageSize && newImmHeight < imageSize) {
-                                    ctx.drawImage(newImm, 0, 0, 300, 300, 10, (120 + exHeight), imageSize, imageSize);
-                                } else if (newImmWidth < imageSize && newImmHeight < imageSize) {
-                                    ctx.drawImage(newImm, 0, 0, newImmWidthDp, newImmHeightDp, (10), (120), imageSize, imageSize);
-                                } else if (newImmWidth > imageSize && newImmHeight == imageSize) {
-                                    ctx.drawImage(newImm, 0, 0, 300, 300, (10 + exWidth), 120, imageSize, imageSize);
-                                } else if (newImmWidth == imageSize && newImmHeight > imageSize) {
-                                    ctx.drawImage(newImm, 0, 0, 300, 300, 10, (120 + exHeight), imageSize, imageSize);
-                                } else if (newImmWidth > imageSize && newImmHeight > imageSize) {
-                                    ctx.drawImage(newImm, 0, 0, newImmWidthDp, newImmHeightDp, (10), (120), imageSize, imageSize);
-                                } else if (newImmWidth > imageSize && newImmHeight < imageSize) {
-                                    ctx.drawImage(newImm, 0, 0, newImmWidthDp, newImmHeightDp, (10), (120), imageSize, imageSize);
-                                } else if (newImmWidth < imageSize && newImmHeight > imageSize) {
-                                    ctx.drawImage(newImm, 0, 0, newImmWidthDp, newImmHeightDp, (10), (120), imageSize, imageSize);
-                                }
-
-                                //ctx.clearRect(0, 0, canvas.width, canvas.height);
-                                //ctx.drawImage(newImm, 0, 0, 300, 300, 10, 80, 250, 250);
-                                fullimg = canvas.toDataURL("image/png");
-                            }
-
-                            function overalayColor(himg, color) {
-                                fullimg = himg[0];
-                                img = new Image();
-                                img.src = himg.src;
-                                selectImg = himg;
-                                canvas.width = 1000;
-                                canvas.height = 667;
-
-                                ctxx.clearRect(0, 0, canvasx.width, canvasx.height);
-                                ctxx.drawImage(selectImg, 0, 0, selectImg.naturalWidth, selectImg.naturalHeight, 0, 0, selectImg.width, selectImg.height);
-                                originalPixels = ctxx.getImageData(0, 0, selectImg.width, selectImg.height);
-                                currentPixels = ctxx.getImageData(0, 0, selectImg.width, selectImg.height);
-
-                                selectImg.onload = null;
-                                fillColor(color);
-                            }
-                            overalayColor(document.getElementById('ball_logo'), "#ffd700");
-                            var imgsrc = canvas.toDataURL("image/png", 1.0);
-                            var geometry = new THREE.SphereGeometry(1, 500, 500);
-                            var textur = new THREE.TextureLoader();
-                            textur.load(
-                                fullimg,
-                                function(texture) {
-                                    var material = new THREE.MeshPhongMaterial({
-                                        map: texture,
-                                        transparent: true
-                                    });
-                                    material.map.needsUpdate = true;
-                                    var mysphere = new THREE.Mesh(geometry, material);
-                                    mysphere.rotation.x = 0.1;
-                                    mysphere.rotation.y = -5.0;
-                                    mysphere.rotation.z = -1;
-                                    scene.add(mysphere);
-                                },
-                                function(xhr) {
-                                    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-                                },
-                                function(xhr) {
-                                    console.log('An error happened');
-                                }
-                            );
-                        };
-                        reader.readAsDataURL(input.files[0]);
-                    }
-                }
-                $("#upload").change(function() {
-                    readURL(this);
-                });
-
-                function animate() {
-                    requestAnimationFrame(animate);
-                    renderer.render(scene, camera);
-                }
-            }, 100);
-        });
 
         $scope.color = [{
             colr: "#f5b122"
