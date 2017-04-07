4a0ebb7bbd79fd76d4b8fbd131a87d68fcf86135
wohlig <chintan@wohlig.com>
2017-04-07 12:11:08 +0530

    added

--- js/customui.js
+++ js/customui.js
@@ -701,35 +701,34 @@
                   }
               };
 
-              var odiForBase = ["", ""];
-              var odiForTrim1 = ["", ""];
-              var odiForTrim2 = ["", ""];
+              var odiForBase = ["Googly (SUB)", "Grubber (SUB)","Plumb (SUB)","Floater (SUB)","Pace (C&S)","Seam (C&S)","Dipper (C&S)","Flipper (C&S)","Drift (C&S)"];
+              var odiForTrim1 = ["Googly (SUB)", "Grubber (SUB)","Plumb (SUB)","Floater (SUB)","Pace (C&S)","Seam (C&S)","Dipper (C&S)","Flipper (C&S)","Drift (C&S)","Whites"];
+              var odiForTrim2 = ["Googly (SUB)", "Grubber (SUB)","Plumb (SUB)","Floater (SUB)"];
 
 
 
-              var troForBase = ["", ""];
-              var troForTrim1 = ["", ""];
-              var troForTrim2 = ["", ""];
+              var troForTrim2 = ["Scamper"];
 
               $scope.imagesPositions = function () {
                   if ($scope.design.type == "odi") {
                       $scope.design.leftSleeveLogoBack = _.cloneDeep($scope.design.leftSleeveLogo);
                       $scope.design.rightSleeveLogoBack = _.cloneDeep($scope.design.rightSleeveLogo);
                   }
-                  if ($scope.design.type == "gloves" || $scope.design.type == "odi") {
-                      return allOtherImages[$scope.design.type][$scope.design.design.name];
-                  } else {
-                      return allOtherImages[$scope.design.type];
-                  }
+                 
                   if ($scope.design.type == "odi") {
                       $scope.design.isBase = _.findIndex(odiForBase, $scope.design.design.name);
                       $scope.design.isTrim1 = _.findIndex(odiForTrim1, $scope.design.design.name);
                       $scope.design.isTrim2 = _.findIndex(odiForTrim2, $scope.design.design.name);
                   }
                   if ($scope.design.type == "trousers") {
-                      $scope.design.isBase = _.findIndex(troForBase, $scope.design.design.name);
-                      $scope.design.isTrim1 = _.findIndex(troForTrim1, $scope.design.design.name);
-                      $scope.design.isTrim2 = _.findIndex(troForTrim2, $scope.design.design.name);
+                      $scope.design.isBase = true
+                      $scope.design.isTrim1 = true
+                      $scope.design.isTrim2 = true
+                  }
+                   if ($scope.design.type == "gloves" || $scope.design.type == "odi") {
+                      return allOtherImages[$scope.design.type][$scope.design.design.name];
+                  } else {
+                      return allOtherImages[$scope.design.type];
                   }
               };
               $scope.showFront = function () {
